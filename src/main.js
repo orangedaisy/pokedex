0cef3a7a3eb486b161d93a86361cc483f2eb2488
Robert Showalter <robert@orangedaisy.co>
2020-04-08 16:14:47 -0400

    Configure vue-router for pokedex navigation

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
+import Pokedex from './components/Pokedex';
 import BackButton from './components/entry/BackButton';
 import Header from './components/entry/Header';
 import Type from './components/entry/Type';
 import Attributes from './components/entry/Attributes';
 import router from './router'
 
+Vue.component('pokedex', Pokedex);
 Vue.component('entryBackButton', BackButton);
 Vue.component('entryHeader', Header);
 Vue.component('entryType', Type);
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Pokedex from '../components/Pokedex.vue'
+import Pikachu from '../Pikachu.vue'
+import Charmander from '../Charmander.vue'
+import Bulbasaur from '../Bulbasaur.vue'
+import Cubone from '../Cubone.vue'
+import Eevee from '../Eevee.vue'
+import Jigglypuff from '../Jigglypuff.vue'
+import Meowth from '../Meowth.vue'
+import Squirtle from '../Squirtle.vue'
+import Togepi from '../Togepi.vue'
+import Piplup from '../Piplup.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-  },
-  {
-  }
+  { path: '', component: Pokedex },
+  { path: '/pikachu', component: Pikachu },
+  { path: '/charmander', component: Charmander },
+  { path: '/bulbasaur', component: Bulbasaur },
+  { path: '/cubone', component: Cubone },
+  { path: '/eevee', component: Eevee },
+  { path: '/jigglypuff', component: Jigglypuff },
+  { path: '/meowth', component: Meowth },
+  { path: '/squirtle', component: Squirtle },
+  { path: '/togepi', component: Togepi },
+  { path: '/piplup', component: Piplup },
 ]
 
 const router = new VueRouter({
