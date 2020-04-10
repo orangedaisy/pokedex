acc9287fbeeb510e1ce77bd07f3e879bea8e399e
Robert Showalter <robert@orangedaisy.co>
2020-04-09 21:13:52 -0400

    Add empty pokedex view

---
+++ src/components/EmptyPokedex.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="text">
+      <p>Looks like you haven't caught any POKéMON yet!</p>
+      <p>
+        When you see your first POKéMON, use the <strong>Catch!</strong> button
+        below to add them to your POKéDEX. Every POKéMON you catch in the wild
+        will be displayed here.
+      </p>
+    </div>
+  </div>
+</template>
